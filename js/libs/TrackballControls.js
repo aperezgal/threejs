d8d706e24224f6893a390ccd90fa09a02b107002
Alberto Pérez Galiano <alberto.lipe@gmail.com>
2013-02-06 18:04:23 +0100

    add a basic camera transition

--- js/libs/TrackballControls.js
+++ js/libs/TrackballControls.js
@@ -440,7 +440,7 @@ THREE.TrackballControls = function ( object, domElement ) {
 
 	this.domElement.addEventListener( 'mousedown', mousedown, false );
 
-	this.domElement.addEventListener( 'mousewheel', mousewheel, false );
+	//this.domElement.addEventListener( 'mousewheel', mousewheel, false );
 	this.domElement.addEventListener( 'DOMMouseScroll', mousewheel, false ); // firefox
 
 	this.domElement.addEventListener( 'touchstart', touchstart, false );
